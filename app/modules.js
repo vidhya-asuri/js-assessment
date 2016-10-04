f062f34746dcc5c948089d8cc8f6dcdc09bbb57f
vidhya <vid.sigsoln@gmail.com>
2016-10-04 17:13:35 +0000

    passed tests in modules.js

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,29 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+     var module = {
+       greeting: str1,
+       name: str2,
+       sayIt: function(){
+         return this.greeting + ', ' + this.name; 
+       } 
+    };
+    return module;
   }
 };
+
+
+/*
+    var module = modulesAnswers.createModule('hello', 'matt');
+
+    expect(module.sayIt).to.be.a('function');
+    expect(module.name).to.eql('matt');
+    expect(module.greeting).to.eql('hello');
+    expect(module.sayIt()).to.eql('hello, matt');
+
+    module.name = 'katniss';
+    module.greeting = 'hi';
+    expect(module.name).to.eql('katniss');
+    expect(module.greeting).to.eql('hi');
+    expect(module.sayIt()).to.eql('hi, katniss');
+*/
