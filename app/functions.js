4c779bdafd20f42857ab695b698fd3d14eb77bc9
vidhya <vid.sigsoln@gmail.com>
2016-10-04 15:51:50 +0000

    passed - partial using variable number of arguments

--- app/functions.js
+++ app/functions.js
@@ -117,6 +117,17 @@ exports.functionsAnswers = {
   },
 
   partialUsingArguments: function(fn) {
+  // A reference to the Array#slice method.
+    var slice = Array.prototype.slice;
+  // Convert arguments object to an array, removing the first argument.
+    var args = slice.call(arguments, 1);
+
+    return function() {
+       // Invoke the originally-specified function, passing in all originally-
+       // specified arguments, followed by any just-specified arguments.
+       return fn.apply(this, args.concat(slice.call(arguments, 0)));
+    };
+
 
   },
   // http://benalman.com/news/2012/09/partial-application-in-javascript/
