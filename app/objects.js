6b944249f6a352df97ec7cff87cc5872b07ed5c0
vidhya <vid.sigsoln@gmail.com>
2016-10-14 02:27:46 +0000

    recursion.js listFiles function and Fibonacci functions work, others not complete

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    var allProperties = Object.getOwnPropertyNames(obj);
+    var result = [];
+    for(var i =0; i < allProperties.length; i++){
+      var prop = allProperties[i] + ': ' + obj[allProperties[i]];
+      result.push(prop);
+    } 
+    return result; 
   }
 };
+
